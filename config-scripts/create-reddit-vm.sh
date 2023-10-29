3bf0105f0b6a5564422aa19c72969b9bc20ad8e6
mickpav <57882789+mickpav@users.noreply.github.com>
2023-10-30 02:34:26 +1000

    Update create-reddit-vm.sh

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -6,4 +6,4 @@ yc compute instance create \
 --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
 --metadata serial-port-enable=1 \
 --zone=ru-central1-a \
---metadata-from-file user-data=startup.yaml
+
